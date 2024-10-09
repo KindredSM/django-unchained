4e4b91174c31ce7de57abc1f9f69f956a87e75a3
Kindred <kindred@kindredsalway.com>
2024-10-09 17:05:39 +0100

    change directories, user, remove commands

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='aopskdfopakodfkaeopf]')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -48,11 +49,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.sites',
+    'django.contrib.sites',  
     'polls',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
+    'allauth.socialaccount',  
 ]
 
 MIDDLEWARE = [
