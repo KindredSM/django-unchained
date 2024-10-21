fc29f151e2a7a041e0ee27faa077d583b3615c61
Kindred <kindred@kindredsalway.com>
2024-10-21 11:52:09 +0100

    update settings and gunicorn

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,11 +49,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'django.contrib.sites',  
     'polls',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',  
+
 ]
 
 MIDDLEWARE = [
